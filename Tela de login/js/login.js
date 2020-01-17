feba639ae383a2fbd900e5fa87ad4f4c54140297
SidSan97 <sidnei1.8santiago@hotmail.com>
2020-01-17 15:17:10 -0300

    Add files via upload
    Primeira tela de login, ainda em mudanças

---
+++ Tela de login/js/login.js
@@ -0,0 +1,44 @@
+document.getElementById('bt1').onmouseenter = function() {mouseEnter()};
+document.getElementById('bt1').onmouseleave = function() {mouseLeave()};
+
+document.getElementById('bt2').onmouseenter = function() {mouseEnter1()};
+document.getElementById('bt2').onmouseleave = function() {mouseLeave1()};
+
+document.getElementById('bt3').onmouseenter = function() {mouseEnter2()};
+document.getElementById('bt3').onmouseleave = function() {mouseLeave2()};
+
+    function mouseEnter() 
+     {
+      document.getElementById('bt1').style.background = 'black';
+      document.getElementById('bt1').style.color = 'white';
+     }
+
+     function mouseEnter1()
+     {
+      document.getElementById('bt2').style.background = 'black';
+      document.getElementById('bt2').style.color = 'white';
+     }
+     
+     function mouseLeave()
+     {
+       document.getElementById('bt1').style.background = 'rgba(255, 255, 255, 0.993)';
+       document.getElementById('bt1').style.color = 'black';
+     }
+
+     function mouseLeave1()
+     {
+      document.getElementById('bt2').style.background = 'rgba(255, 255, 255, 0.993)';
+      document.getElementById('bt2').style.color = 'black';
+     }
+
+     function mouseEnter2()
+     {
+      document.getElementById('bt3').style.background = 'black';
+      document.getElementById('bt3').style.color = 'white';
+     }
+
+     function mouseLeave2()
+     {
+      document.getElementById('bt3').style.background = 'rgba(255, 255, 255, 0.993)';
+      document.getElementById('bt3').style.color = 'black';
+     }
